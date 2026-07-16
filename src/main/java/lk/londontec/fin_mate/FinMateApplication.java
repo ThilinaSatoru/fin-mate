c0a4662eb43d2c8474c92c0eb8c01339c84e29d6
ThilinaSatoru <satoru.thilina@gmail.com>
2026-07-16 09:12:22 +0530

    spring init

---
+++ src/main/java/lk/londontec/fin_mate/FinMateApplication.java
@@ -0,0 +1,13 @@
+package lk.londontec.fin_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinMateApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FinMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/londontec/fin_mate/FinMateApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.londontec.fin_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
